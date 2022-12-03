c88de1750b66e5d5effaaf980765424665e27da7
dddowon <dowon4095@naver.com>
2022-12-03 22:22:22 +0900

    feat: 위도, 경도 알아내는 함수 추가

--- WeatherApp/WeatherApp/Controller/ViewController.swift
+++ WeatherApp/WeatherApp/Controller/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    let locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        updateLocation()
     }
 
 
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    func updateLocation() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            let latitude = location.coordinate.latitude
+            let longitude = location.coordinate.longitude
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
+    
+    func getLatitudeLongitude() -> [Double] {
+        let latitude = Double(locationManager.location?.coordinate.latitude ?? 0)
+        let longitude = Double(locationManager.location?.coordinate.longitude ?? 0)
+
+        return [latitude, longitude]
+    }
+}
